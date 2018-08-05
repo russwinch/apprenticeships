58e470586b42ef0336341fb75c1de68cc0f323a8
russ <russell.winch@gmail.com>
2018-08-05 16:15:02 +0100

    Implement JSON output for matching.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 1. Web scraping
----
+===
 a. All results from https://www.instituteforapprenticeships.org/apprenticeship-standards/ are
 scraped and stored in the file step1a.json
 
@@ -18,3 +18,15 @@ To run: ```scrapy runspider scrape_finda.py -o step1b.json```
 
 TODO:
 - should be combined into file 1 with a function to run both to json files
+---
+2. Matching datasets
+===
+a. The two datasets are iterated over and matched on:
+- a composite key comprised of the lowercase name and the number of the level
+- the url on the Institiue for Apprenticeships site (some pages on the Find Apprenticeship Training site have
+a link to the corresponding page on the IfA).
+
+The url match is the more effective method, with the key matching just picking
+up 1 more apprenticeship.
+
+To run: ```python matching.py matchonly```
--- matching.py
+++ matching.py
@@ -1,11 +1,18 @@
 import json
+import sys
 
 
-def load_file(filepath):
+def load_json_file(filepath):
     """Opens a json file and converts it to python object."""
     with open(filepath, mode='r') as f:
         apprenticeship_dict = json.load(f)
     return apprenticeship_dict
+
+
+def output_json_file(*, data, filepath):
+    """Converts a python object to json and writes the file."""
+    with open(filepath, mode='w') as f:
+        json.dump(data, f)
 
 
 def match_apps(*, ifa, finda):
@@ -17,6 +24,7 @@ def match_apps(*, ifa, finda):
     :ifa: python object containing data from the Institute for Apprenticeships
     :finda: python object containing data from Find Apprenticeships Training
     """
+    matches = {}
     match_count = 0
     for a in ifa:
         a_key = f"{a['name']}: {a['level'][:1]}".lower()
@@ -25,8 +33,9 @@ def match_apps(*, ifa, finda):
             if a_key == b_key or a['url'] == b['ifa_url']:
                 # we have a match!
                 match_count += 1
-                print(a_key, ":", b_key)
-                # TODO: output to JSON here
+                matches[a_key] = b_key
+    matches['total'] = match_count
+    output_json_file(data=matches, filepath='step2a.json')
     print(f"Total matches: {match_count}")
 
 
@@ -34,7 +43,15 @@ if __name__ == '__main__':
     ifa_file = 'step1a.json'
     findapp_file = 'step1b.json'
 
-    ifa = load_file(ifa_file)
-    finda = load_file(findapp_file)
+    ifa = load_json_file(ifa_file)
+    finda = load_json_file(findapp_file)
 
     match_apps(ifa=ifa, finda=finda)
+    try:
+        if sys.argv[1] == 'matchonly':
+            sys.exit()
+    except IndexError:
+        # no arguments were passed when the script was called so we can ignore
+        pass
+
+    # continue on to the dedupe...
